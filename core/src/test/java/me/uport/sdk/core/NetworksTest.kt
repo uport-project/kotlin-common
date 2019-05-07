7451901c0574e21f69363f16a350d0b0c80eab73
Mircea Nistor <mirceanis@gmail.com>
2019-05-07 16:04:17 +0300

    add some simple tests for `Networks` object

---
+++ core/src/test/java/me/uport/sdk/core/NetworksTest.kt
@@ -0,0 +1,50 @@
+package me.uport.sdk.core
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isInstanceOf
+import org.junit.Test
+
+class NetworksTest {
+
+    @Test
+    fun `registering an existing network overrides it`() {
+        //override default registry address for mainnet
+        Networks.registerNetwork(
+            Networks.mainnet.copy(rpcUrl = "http://localhost:8545")
+        )
+
+        assertThat(Networks.mainnet.rpcUrl).isEqualTo("http://localhost:8545")
+        assertThat(Networks.get("0x1").rpcUrl).isEqualTo("http://localhost:8545")
+    }
+
+    @Test
+    fun `registering a new network works`() {
+        //register a clone of mainnet with different id
+        Networks.registerNetwork(
+            Networks.mainnet.copy(networkId = "0x4321")
+        )
+
+        Networks.registerNetwork(
+            EthNetwork(
+                name = "local",
+                networkId = "0x1234",
+                rpcUrl = "http://localhost:8545",
+                ethrDidRegistry = "0xdca7ef03e98e0dc2b855be647c39abe984fcf21b"
+            )
+        )
+
+        //other fields match
+        assertThat(Networks.get("0x4321").ethrDidRegistry)
+            .isEqualTo(Networks.mainnet.ethrDidRegistry)
+    }
+
+    @Test
+    fun `getting an unknown network throws`() {
+        assertThat {
+            Networks.get("0x1234")
+        }.thrownError {
+            isInstanceOf(IllegalStateException::class)
+        }
+    }
+}
